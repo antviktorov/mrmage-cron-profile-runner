849ed01be05b2582dc02420ca074dbf43e56d225
antonviktorov <antvik85@gmail.com>
2018-02-24 15:46:46 +0300

    Added: licence

--- app/code/community/Mrmage/CronProfileRunner/Helper/Dataflow.php
+++ app/code/community/Mrmage/CronProfileRunner/Helper/Dataflow.php
@@ -1,5 +1,26 @@
 <?php
 /**
+ * MrMage
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject MrMage
+ * that is available through the world-wide-web at this URL:
+ * http://mrmage.com/LICENSE-1.0.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to support@mrmage.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the extension to newer
+ * versions in the future. If you wish to customize the extension for your
+ * needs please refer to http://mrmage.com/ for more information.
+ *
+ * @copyright  Copyright (c) 2016-2018 MrMage company
+ * @license    http://mrmage.com/LICENSE-1.0.html
+ */
+/**
  * Class Mrmage_CronProfileRunner_Helper_Dataflow
  * @author MrMage team <support@mrmage.com>
  */
